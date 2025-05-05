62b01574d66e17d88667db32d295796cf69ed1f9
rohitpalkar73 <rohitpalkar73@gmail.com>
2025-05-05 09:08:29 +0530

    Auto-generated commit

--- src/app/ngrx/data.action.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from "@ngrx/store";
-
-
-export const addData = createAction('[NewData] AddData', props<{ csvData: any }>());
-export const addFirebaseData = createAction('[NewFirebaseData] AddFirebaseData', props<{ csvData: any }>());
-export const addChart = createAction('[NewChart] AddChart', props<{ id: any, property: any }>());
-export const deleteData = createAction('[DeleteData] DeleteData', props<{ id: any }>());
-export const deleteChart = createAction('[DeleteChart] DeleteChart');
